61af69c06b9e0bf3d86397af706d9f3a6b02b8c8
yulia <yulka84@gmail.com>
2021-03-03 15:15:52 +0200

    contac us form validations

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-function submitForm(){}
+function submitForm(){
+    document.getElementsByClassName("contactUsForm")[0].className = "contactUsForm submitted";
+
+    
+
+    if(document.getElementById('email').validity.typeMismatch) {
+        document.querySelector('#email + span.error').style.display = "block";
+        document.querySelector('#email + span.error').textContent = 'קלט לא מתאים';
+    }
+    else if(document.getElementById('email').validity.valueMissing){
+        document.querySelector('#email + span.error').style.display = "block";
+        document.querySelector('#email + span.error').textContent = 'שדה חובה';
+    }
+    else{
+        document.querySelector('#email + span.error').style.display = "none";
+    }
+}
