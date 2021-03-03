79a39c24eff0f5ee8a6efbbe3633b03bed2eeedc
yulia <yulka84@gmail.com>
2021-03-03 17:30:12 +0200

    add sliding image to homepage banner

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
+var images =[
+    "images\\paz_250x110103131473.jpg",
+    "images\\paz_250x110059890678.jpg",
+    "images\\banner_atar.jpg",
+    "images\\250X110668568400.jpg",
+    "images\\ez.jpg"
+];
+
+$(function() {
+    
+    var curImg = 0;
+    setInterval(function () {
+        if(curImg== images.length){
+            curImg = 0;
+        }
+      $(".slidingImage").attr("src",  images[curImg++]);
+    }, 5000);
+});
+
+
 function submitForm(){
     document.getElementsByClassName("contactUsForm")[0].className = "contactUsForm submitted";
-
-    
 
     if(document.getElementById('email').validity.typeMismatch) {
         document.querySelector('#email + span.error').style.display = "block";
